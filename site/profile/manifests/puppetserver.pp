a98096c99893a55c1c68e5234827bba44d2d261a
Bas Wildemann <bas@MacBook-Pro-van-Bas.local>
2018-03-29 19:59:03 +0200

    added eyaml

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -1,6 +1,11 @@
 class profile::puppetserver {
 
   class {'puppetserver':}
+  
+  class { '::puppetserver::hiera::eyaml':
+    require => Class['puppetserver::install'],
+  }
+
 
   file {'/etc/puppetlabs/puppet/autosign.conf':
     ensure  => present,
