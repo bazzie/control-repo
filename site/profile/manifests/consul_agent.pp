7d07a2b6d650f947ee970bc607df847179a401ae
Bas Wildemann <bas@MacBook-Pro-van-Bas.local>
2018-04-03 21:42:42 +0200

    consul agent profile removed datacenter

--- site/profile/manifests/consul_agent.pp
+++ site/profile/manifests/consul_agent.pp
@@ -9,7 +9,6 @@ class profile::consul_agent {
   class { '::consul':
     config_hash => {
       'data_dir'   => '/opt/consul',
-      'datacenter' => 'east-aws',
       'log_level'  => 'INFO',
       'node_name'  => 'agent',
       'retry_join' => ['monitoring.wildemann.nl'],
