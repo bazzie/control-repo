79aa9262864c7387d5821456ec1fc61ec12cc627
Bas Wildemann <bas@MacBook-Pro-van-Bas.local>
2018-04-03 23:12:22 +0200

    consul service puppetserver

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -3,4 +3,9 @@ class profile::puppetdb {
   class {'puppetdb':}  
   class {'puppetdb::master::config':}  
 
+  file {'/etc/consul/service_puppetdb.json':
+    ensure  => present,
+    content => '{"service":{"name":"puppetdb","port":8081}}',
+  }
+
 } 
