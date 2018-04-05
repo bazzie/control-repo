3c675c0653252c2e36418f6963c49ca7a1fe4103
Bas Wildemann <bas@MacBook-Pro-van-Bas.local>
2018-04-05 19:05:02 +0200

    added homey

--- site/profile/manifests/prometheus_server.pp
+++ site/profile/manifests/prometheus_server.pp
@@ -18,8 +18,16 @@ class profile::prometheus_server {
         'scrape_interval' => '5s',
         'scrape_timeout'  => '5s',
         'static_configs'  => [
-           { 'targets' => [ 'monitoring.wildemann.nl:9100', 'test.wildemann.nl:9100', 'puppet.wildemann.nl:9100', 'server.wildemann.nl:9100','homey.wildemann.nl:9414' ],
+           { 'targets' => [ 'monitoring.wildemann.nl:9100', 'test.wildemann.nl:9100', 'puppet.wildemann.nl:9100', 'server.wildemann.nl:9100' ],
              'labels'  => { 'alias'=> 'Node'}
+           }
+        ]
+      },
+     { 'job_name' => 'homey',
+        'scrape_interval' => '15s',
+        'static_configs'  => [
+           { 'targets' => [ 'homey.wildemann.nl:9414' ],
+             'labels'  => { 'alias'=> 'Homey'}
            }
         ]
       }
