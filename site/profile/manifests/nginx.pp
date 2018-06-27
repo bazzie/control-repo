1a756bde8f23f4658297affc19c9c5d4fb19ce94
bazzie <bas@wildemann.nl>
2018-06-27 05:37:07 +0000

    typo

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -4,11 +4,20 @@ class profile::nginx {
     ensure => directory,
   }
 
+  file { '/var/www/www.nextworking.nl':
+    ensure => directory,
+  }
+
   class { 'nginx': }
 
   nginx::resource::server { 'www.wildemann.nl':
     www_root => '/var/www/www.wildemann.nl',
   }
-  
+ 
+  nginx::resource::server { 'www.nextworking.nl':
+    www_root => '/var/www/www.nextworking.nl',
+  }
+
+ 
 
 }
