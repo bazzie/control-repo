13999fd4c25b4c1bff464aea6209b3d8a3aecaa3
bazzie <bas@wildemann.nl>
2018-07-17 18:05:28 +0000

    puppet metrics dashboard

--- site/profile/manifests/puppet_metrics.pp
+++ site/profile/manifests/puppet_metrics.pp
@@ -1,10 +1,11 @@
 class profile::puppet_metrics {
 
   class { 'puppet_metrics_dashboard':
-    configure_telegraf  => true,
-    enable_telegraf     => true,
-    master_list         => ['puppet.wildemann.nl'],
-    puppetdb_list       => ['puppet.wildemann.nl'],
+    configure_telegraf     => true,
+    enable_telegraf        => true,
+    master_list            => ['puppet.wildemann.nl'],
+    puppetdb_list          => ['puppet.wildemann.nl'],
+    add_dashboard_examples => true,
   }
 
 
