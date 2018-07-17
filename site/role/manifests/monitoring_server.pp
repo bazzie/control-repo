7300639ef319fff7092aaae8a94bef267e29c80a
bazzie <bas@wildemann.nl>
2018-07-17 18:02:08 +0000

    puppet metrics dashboard

--- site/role/manifests/monitoring_server.pp
+++ site/role/manifests/monitoring_server.pp
@@ -3,7 +3,7 @@ class role::monitoring_server {
   include profile::consul_server
   include profile::prometheus_server
   include prometheus::node_exporter
-  include profile::grafana
+  #include profile::grafana
   include profile::puppet_metrics
 
 }
