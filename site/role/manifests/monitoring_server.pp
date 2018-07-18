96062f9744b91a62e7aaa1b636d78b1d652b9a78
bazzie <bas@wildemann.nl>
2018-07-18 17:46:49 +0000

    removed puppet metrics dashboard

--- site/role/manifests/monitoring_server.pp
+++ site/role/manifests/monitoring_server.pp
@@ -3,7 +3,6 @@ class role::monitoring_server {
   include profile::consul_server
   include profile::prometheus_server
   include prometheus::node_exporter
-  #include profile::grafana
-  include profile::puppet_metrics
+  include profile::grafana
 
 }
