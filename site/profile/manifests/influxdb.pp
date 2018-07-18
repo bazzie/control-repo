9588a7f9df758ec9e31fa5e62f4d89b389aca80b
bazzie <bas@wildemann.nl>
2018-07-18 18:12:01 +0000

    added influxdb

---
+++ site/profile/manifests/influxdb.pp
@@ -0,0 +1,5 @@
+class profile::influxdb {
+
+  class {'influxdb::server':}
+
+}
--- site/role/manifests/monitoring_server.pp
+++ site/role/manifests/monitoring_server.pp
@@ -3,6 +3,7 @@ class role::monitoring_server {
   include profile::consul_server
   include profile::prometheus_server
   include prometheus::node_exporter
+  include profile::influxdb
   include profile::grafana
 
 }
